fc4f3c3b39b054e0eba85069d7824a0bcbff2df7
Elakkiya.Muthukumarasamy <elakkiya.muthukumarasamy@syncfusion.com>
2026-04-29 11:21:25 +0530

    Sample updated

--- GettingStarted/MainPage.xaml.cs
+++ GettingStarted/MainPage.xaml.cs
@@ -1,14 +1,11 @@
-﻿using Syncfusion.UI.Xaml.Spreadsheet;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,19 +26,8 @@ namespace GettingStarted
         public MainPage()
         {
             this.InitializeComponent();
-            OpenWorkbook();
-        }
-
-        async void OpenWorkbook()
-        {
             Stream fileStream = typeof(MainPage).GetTypeInfo().Assembly.GetManifestResourceStream("GettingStarted.Assets.GettingStarted.xlsx");
             this.spreadsheet.Open(fileStream);
-            //var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            //string resourcePath = "GettingStarted.Assets.GettingStarted.xlsx";
-            //using (var fileStream = assembly.GetManifestResourceStream(resourcePath))
-            //{
-            //    await this.spreadsheet.Open(fileStream);
-            //}
         }
     }
 }
